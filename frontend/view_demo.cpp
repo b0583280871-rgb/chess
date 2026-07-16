78f16787a91c818249a7a4de0b6cff487873cfaa
Bracha winegarten <B0583280871@gmail.com>
2026-07-16 11:53:18 +0300

    Sending a starting board to the BoarsPerser layer

--- frontend/view_demo.cpp
+++ frontend/view_demo.cpp
@@ -1,22 +1,28 @@
 #include "view/GameSnapshot.hpp"
 #include "view/Renderer.hpp"
 #include "../backend/io/BoardParser.hpp"
+#include "../backend/io/BoardFormat.hpp"
 
 #include <iostream>
 
 int main() {
     try {
+        const std::string boardText =
+            io_format::SECTION_BOARD + "\n"
+            "bR bN bB bQ bK bB bN bR\n"
+            "bP bP bP bP bP bP bP bP\n"
+            ". . . . . . . .\n"
+            ". . . . . . . .\n"
+            ". . . . . . . .\n"
+            ". . . . . . . .\n"
+            "wP wP wP wP wP wP wP wP\n"
+            "wR wN wB wQ wK wB wN wR\n" +
+            io_format::SECTION_COMMANDS + "\n";
+
+        Sections sections = parseSections(boardText);
+
         GameState st;
-        st.board = parseBoard({
-            "bR bN bB bQ bK bB bN bR",
-            "bP bP bP bP bP bP bP bP",
-            ". . . . . . . .",
-            ". . . . . . . .",
-            ". . . . . . . .",
-            ". . . . . . . .",
-            "wP wP wP wP wP wP wP wP",
-            "wR wN wB wQ wK wB wN wR"
-        });
+        st.board = parseBoard(sections.boardLines);
 
         GameSnapshot snapshot = buildSnapshot(st);
         Img frame = renderFrame(snapshot);
