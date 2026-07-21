74a3877d834058d2c79d9086e43ab48905e75347
Bracha winegarten <B0583280871@gmail.com>
2026-07-21 09:33:47 +0300

    Remove unused loadPieceIdleSprite function from SpriteLoader

--- frontend/view/SpriteLoader.hpp
+++ frontend/view/SpriteLoader.hpp
@@ -9,8 +9,6 @@ namespace SpriteLoader {
 
     inline const std::string PIECES_ROOT = "assets/pieces_classic/";
 
-    Img loadPieceIdleSprite(const std::string& pieceCode);
-
     std::string pieceCodeFromPiece(Color color, Kind kind);
 
     Img& getCachedPieceSprite(const std::string& pieceCode, const std::string& state);
